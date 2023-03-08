f97f38792213badebc92545e532add19ec4e67fc
xinyang <1349746992@qq.com>
2023-03-09 00:30:25 +0800

    1.0.05 commit

--- src/main/java/com/blog/web/IndexController.java
+++ src/main/java/com/blog/web/IndexController.java
@@ -8,8 +8,8 @@ import org.springframework.web.bind.annotation.PathVariable;
 @Controller
 public class IndexController {
 
-    @GetMapping("/{id}/{name}")
-    public String index(@PathVariable Integer id,@PathVariable String name){
+    @GetMapping("/")
+    public String index(){
 //        int i = 9/0;
 //        String blog = null;
 //        if(blog == null){
