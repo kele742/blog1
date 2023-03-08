a6fb85a7e9d84a45e24ae29b46566f9069bd5308
xinyang <1349746992@qq.com>
2023-03-08 23:40:19 +0800

    1.0.04 commit

---
+++ src/main/java/com/blog/aspect/LogAspect.java
@@ -0,0 +1,32 @@
+package com.blog.aspect;
+
+import org.aspectj.lang.annotation.*;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+
+@Aspect
+@Component
+public class LogAspect {
+
+    public final Logger logger = LoggerFactory.getLogger(this.getClass());
+    @Pointcut("execution(* com.blog.web.*(..))")
+    public void log(){
+
+    }
+
+    @Before("log()")
+    public void doBefore(){
+        logger.info("----------doBefore-----------");
+    }
+
+    @After("log()")
+    public  void doAfter(){
+        logger.info("----------doAfter-----------");
+    }
+
+    @AfterReturning(returning = "result",pointcut = "log()")
+    public void doAfterReturn(Object result){
+        logger.info("Result : {}" + result);
+    }
+}
