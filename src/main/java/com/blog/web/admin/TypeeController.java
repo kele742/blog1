a1c4b2ccbc656e1bc35ed14e1ce6c6da8f348f33
xinyang <1349746992@qq.com>
2023-03-14 23:54:33 +0800

    1.0.15 commit

--- src/main/java/com/blog/web/admin/TypeeController.java
+++ src/main/java/com/blog/web/admin/TypeeController.java
@@ -3,6 +3,7 @@ package com.blog.web.admin;
 import com.blog.service.TypeService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -15,7 +16,7 @@ public class TypeeController {
     private TypeService typeService;
 
     @GetMapping("/types")
-    public String list(Pageable pageable){
+    public String list(@PageableDefault(size = 10,sort = {"id"}) Pageable pageable){
 
         typeService.listType(pageable);
         return "admin/types";
