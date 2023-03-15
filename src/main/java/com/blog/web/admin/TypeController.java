f2e50434072f60232f3b1e15810fd613f2bc97ec
xinyang <1349746992@qq.com>
2023-03-16 00:15:56 +0800

    1.0.16 commit

--- src/main/java/com/blog/web/admin/TypeController.java
+++ src/main/java/com/blog/web/admin/TypeController.java
@@ -1,5 +1,6 @@
 package com.blog.web.admin;
 
+import com.blog.pojo.Type;
 import com.blog.service.TypeService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Pageable;
@@ -8,7 +9,9 @@ import org.springframework.data.web.PageableDefault;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @Controller
 @RequestMapping("/admin")
@@ -18,10 +21,29 @@ public class TypeController {
     private TypeService typeService;
 
     @GetMapping("/types")
-    public String list(@PageableDefault(size = 10,sort = {"id"},direction = Sort.Direction.DESC)
+    public String list(@PageableDefault(size = 3,sort = {"id"},direction = Sort.Direction.ASC)
                                    Pageable pageable, Model model){
 
         model.addAttribute("page",typeService.listType(pageable));
         return "admin/types";
+    }
+    @GetMapping("/types/input")
+    public String input(){
+
+
+
+        return "admin/types-input";
+    }
+
+    @PostMapping("/types")
+    public String post(Type type, RedirectAttributes attributes){
+        Type t = typeService.saveType(type);
+        if( t == null){
+            //如果保存成功给一个提示
+            attributes.addFlashAttribute("message","操作失败");
+        }else {
+            attributes.addFlashAttribute("message","操作成功");
+        }
+        return "redirect:/admin/types";
     }
 }
