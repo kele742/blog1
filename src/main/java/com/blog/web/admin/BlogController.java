9714c4b82f2f95f7ca5ca9706b5e86931834c497
xinyang <1349746992@qq.com>
2023-03-20 23:07:20 +0800

    1.0.19 commit

--- src/main/java/com/blog/web/admin/BlogController.java
+++ src/main/java/com/blog/web/admin/BlogController.java
@@ -1,6 +1,13 @@
 package com.blog.web.admin;
 
+import com.blog.pojo.Blog;
+import com.blog.service.BlogService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -8,8 +15,13 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping("/admin")
 public class BlogController {
 
+    @Autowired
+    private BlogService blogService;
+
     @GetMapping("/blogs")
-    public String blogs(){
+    public String blogs(@PageableDefault(size = 2,sort = {"updateTime"},direction = Sort.Direction.DESC)
+                                    Pageable pageable, Blog blog, Model model){
+        model.addAttribute("page",blogService.listBlog(pageable,blog));
         return "admin/blogs";
     }
 }
