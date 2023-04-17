7c11abdb79d156a2b4665b15339a0d84d64a5447
xinyang <1349746992@qq.com>
2023-04-18 00:13:33 +0800

    1.1.2 commit

--- src/main/java/com/blog/BlogApplication.java
+++ src/main/java/com/blog/BlogApplication.java
@@ -2,12 +2,20 @@ package com.blog;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class BlogApplication {
-
-    public static void main(String[] args) {
-        SpringApplication.run(BlogApplication.class, args);
-    }
-
+public class BlogApplication extends SpringBootServletInitializer {
+    /**
+     * 修改启动类，继承 SpringBootServletInitializer 并重写 configure 方法
+     */
+        @Override
+        protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+// 注意这里要指向原先用main方法执行的Application启动类
+            return super.configure(builder);
+        }
+        public static void main(String[] args){
+            SpringApplication.run(BlogApplication.class,args);
+        }
 }
